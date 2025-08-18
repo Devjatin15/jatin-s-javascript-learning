87653329f50b7baf8b4c8dd274a7ac187b1c771d
Devjatin15 <jatinkumar1562006@gmail.com>
2025-08-18 03:58:17 +0000

    container config done

---
+++ basics/datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(1 + 2) // we are using nodejs, not browser
+
+console.log(3 + 3); 
+console.log("jatin");
+
+let name = "jatin"
+let age = 3
+let isLoggedIn = false
+let state = null
+
+
+
+/* 
+* number => 2 to power 53 
+* bigint for numbers greater than that
+* string => ""
+* boolean => true/false
+* null => standalone value
+* undefined => no value defined
+* symbol => unique
+* object
+*/
+
+console.log(typeof null);
+// null is an object
+console.log(typeof undefined);
+// it is undefined
+
--- basics/variables.js
+++ basics/variables.js
@@ -4,7 +4,17 @@ let accountEmail = "jatin@google.com"
 var accountPassword = "12345"
 accountCity = "Jaipur"
 
-accountId = 2
+// accountId = 2 // not allowed
 
-console.log(accountId);
+accountEmail = "hc@hc.com"
+accountPassword = "122333"
+accountCity = "Delhi"
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
 
