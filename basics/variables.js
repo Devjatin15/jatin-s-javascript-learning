3b874b53a9ef260fa0e813df73567a8c5ca78ea1
Devjatin15 <jatinkumar1562006@gmail.com>
2025-08-18 03:29:00 +0000

    container config done

---
+++ basics/variables.js
@@ -0,0 +1,10 @@
+const accountId = 144553
+// const means doesnt changes
+let accountEmail = "jatin@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+accountId = 2
+
+console.log(accountId);
+
