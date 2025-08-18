ae2c26e6fd36f8dc531dafb16d34d6b414223925
Devjatin15 <jatinkumar1562006@gmail.com>
2025-08-18 12:35:09 +0000

    container config done

---
+++ basics/conversionOperatio.js
@@ -0,0 +1,30 @@
+let score = undefined
+// value will be not a number(NaN) if 335drd or string
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "jatin"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber + 23);
+console.log(typeof stringNumber);
+
+
+
+
+
