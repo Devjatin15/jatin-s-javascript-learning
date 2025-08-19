5d9fc5c37d9e36ec992c085182cfd4bd51d6b424
Devjatin15 <jatinkumar1562006@gmail.com>
2025-08-19 10:06:57 +0000

    container config done

--- basics/conversionOperatio.js
+++ basics/conversionOperatio.js
@@ -28,7 +28,18 @@ console.log(typeof stringNumber);
 
 let value = 3
 let negValue = -value
-console.log(negValue);
+// console.log(negValue);
+
+let str1 = "hello"
+let str2 = " jatin"
+
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+
+
 
 
 
